2fdfa21a94c8e6d9ac104cbbf3fd86bcc550dee1
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-10-28 08:28:59 +0000

    Got db connection working

--- cmd/gymTracker/main.go
+++ cmd/gymTracker/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/joshibbotson/gym-tracker-backend/internal/db"
 )
@@ -9,6 +10,9 @@ import (
 func main() {
 	db.ConnectDB()
 	defer db.DisconnectDB()
+	// http.HandleFunc("/todo", handler)
+	// put in env variable.
+	http.ListenAndServe(":8888", nil)
 
 	fmt.Println("Running application...")
 	fmt.Println("Starting My Go Project")
--- internal/db/database.go
+++ internal/db/database.go
@@ -6,8 +6,8 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -16,7 +16,8 @@ var Client *mongo.Client
 
 func ConnectDB() {
 	// Updated URI with authSource=admin
-	uri := "mongodb://dev:dev@mongo:27017"
+	// env variable
+	uri := "mongodb://localhost:27017/gym-tracker"
 
 	// Set MongoDB client options
 	clientOptions := options.Client().ApplyURI(uri)
@@ -28,11 +29,11 @@ func ConnectDB() {
 	}
 
 	// Ping the database to verify connection
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := client.Ping(ctx, nil); err != nil {
-		log.Fatal("Failed to ping:", err)
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+		panic(err)
 	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
 	fmt.Println("Connected to MongoDB!")
 	Client = client
