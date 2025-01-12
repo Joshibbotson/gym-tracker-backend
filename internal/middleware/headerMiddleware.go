fe5b2a0980d3328ed3aa633432008c1562782af7
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-01-12 11:27:51 +0000

    minor header middleware update

--- internal/middleware/headerMiddleware.go
+++ internal/middleware/headerMiddleware.go
@@ -13,18 +13,21 @@ func HeaderMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		env := os.Getenv("GO_ENV")
 
 		// Set allowed origins based on the environment
-		var allowedOrigin string
+		var allowedOrigins []string
 		if env == "production" {
-			// In production, replace this with your frontend's HTTPS URL
-			allowedOrigin = "https://gym-commits.netlify.app" // Replace with actual HTTPS frontend URL
+			allowedOrigins = []string{"https://gym-commits.netlify.app"}
 		} else {
-			// For local development, allow the local IP address with HTTP or HTTPS
-			allowedOrigin = "http://localhost:4200, http://5.133.46.201:4200"
+			allowedOrigins = []string{"http://localhost:4200", "http://5.133.46.201:4200"}
 		}
-		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+
+		origin := r.Header.Get("Origin")
+		if contains(allowedOrigins, origin) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Handle preflight requests (OPTIONS method)
 		if r.Method == http.MethodOptions {
@@ -34,4 +37,13 @@ func HeaderMiddleware(next http.HandlerFunc) http.HandlerFunc {
 
 		next.ServeHTTP(w, r)
 	}
+}
+
+func contains(origins []string, target string) bool {
+	for _, o := range origins {
+		if o == target {
+			return true
+		}
+	}
+	return false
 }
