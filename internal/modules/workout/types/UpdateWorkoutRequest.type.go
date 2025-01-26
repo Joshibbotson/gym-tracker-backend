6399d93b6fa01054955b959f3355c2c61db3661a
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2025-01-26 20:03:42 +0000

    hip measurements

--- internal/modules/workout/types/UpdateWorkoutRequest.type.go
+++ internal/modules/workout/types/UpdateWorkoutRequest.type.go
@@ -16,6 +16,7 @@ type UpdateWorkoutRequest struct {
 	CaloriePhase *c.CaloriePhase    `json:"caloriePhase,omitempty" bson:"caloriePhase,omitempty"`
 	ChestSize    *float64           `json:"chestSize,omitempty" bson:"chestSize,omitempty"`
 	WaistSize    *float64           `json:"waistSize,omitempty" bson:"waistSize,omitempty"`
+	HipSize      *float64           `json:"hipSize,omitempty" bson:"hipSize,omitempty"`
 	BicepSize    *float64           `json:"bicepSize,omitempty" bson:"bicepSize,omitempty"`
 	ForearmSize  *float64           `json:"forearmSize,omitempty" bson:"forearmSize,omitempty"`
 	ThighSize    *float64           `json:"thighSize,omitempty" bson:"thighSize,omitempty"`
